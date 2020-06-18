081de8dc5a230dda74e9ee64118225ca9a3bbc50
Jose Elmer Chamagua Pena <echamagu@bancoagricola.com.sv>
2020-06-17 18:37:59 -0600

    first commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.junit.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void posJira() {
+		System.out.println("posJira");
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+	
+}
